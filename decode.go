5b0dad8939f8b938ca696405f0b248c91802f142
valsorym <valsorym.e@gmail.com>
2020-02-19 21:04:17 +0200

    Make decoder with: intX, uintX, floatX and bool support

---
+++ decode.go
@@ -0,0 +1,404 @@
+package env
+
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+const TagName = "env"
+
+// getInt64 to parse string and get int64 value or error.
+// Returns `0` for empty string.
+func getInt64(value string) (int64, error) {
+	var (
+		r   int64
+		err error
+	)
+
+	if len(value) != 0 {
+		r, err = strconv.ParseInt(value, 10, 64)
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	return r, nil
+}
+
+// getUint64 to parse string and get uint64 value or error.
+// Returns `0` for empty string.
+func getUint64(value string) (uint64, error) {
+	var (
+		r   uint64
+		err error
+	)
+
+	if len(value) != 0 {
+		r, err = strconv.ParseUint(value, 10, 64)
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	return r, nil
+}
+
+// getFloat64 to parse string and get float64 value or error.
+// Returns `0` for empty string.
+func getFloat64(value string) (float64, error) {
+	var (
+		r   float64
+		err error
+	)
+
+	if len(value) != 0 {
+		r, err = strconv.ParseFloat(value, 64)
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	return r, nil
+}
+
+// parseTag returns tag parameters as [NAME[, SEP]] where
+//     NAME variable name in the environment;
+//     SEP  separator for the list (only for arrays and slices).
+func parseTag(tagValue, defaultName, defaultSep string) (name, sep string) {
+	var data = strings.Split(tagValue, ",")
+
+	switch len(data) {
+	case 0:
+		name, sep = defaultName, defaultSep
+	case 1:
+		name, sep = strings.TrimSpace(data[0]), defaultSep
+	default: // more then 1
+		name, sep = strings.TrimSpace(data[0]), strings.TrimSpace(data[1])
+	}
+
+	if len(name) == 0 { // the name must be at least one character
+		name = defaultName
+	}
+
+	if len(sep) == 0 { // the sep must be at least one character
+		sep = defaultSep
+	}
+
+	return
+}
+
+// decodeEnviron gets variables from the environment and sets them by
+// pointer into scope. Returns an error if something went wrong.
+//
+// Supported types: Int, Int8, Int16, Int32, Int64, Uint, Uint8, Uint16,
+// Uint32, Uint64, Bool, Float32, Float64, String, Array, Slice.
+func decodeEnviron(scope interface{}) error {
+	var rv reflect.Value
+
+	// The object must be a pointer.
+	rv = reflect.ValueOf(scope)
+	if rv.Type().Kind() != reflect.Ptr {
+		t := rv.Type()
+		return fmt.Errorf("cannot use scope (type %s) as type *%s "+
+			"in argument to decode", t, t)
+	}
+
+	// Get the value of an object.
+	rv = rv.Elem()
+
+	// Walk through all the fields of the transferred object.
+	for i := 0; i < rv.NumField(); i++ {
+		field := rv.Type().Field(i)
+		name, _ := parseTag(field.Tag.Get(TagName), field.Name, " ")
+
+		// Change value.
+		instance := rv.FieldByName(field.Name)
+		switch field.Type.Kind() {
+		case reflect.Int:
+			err := setInt(&instance, name)
+			if err != nil {
+				return err
+			}
+		case reflect.Int8:
+			err := setInt8(&instance, name)
+			if err != nil {
+				return err
+			}
+		case reflect.Int16:
+			err := setInt16(&instance, name)
+			if err != nil {
+				return err
+			}
+		case reflect.Int32:
+			err := setInt32(&instance, name)
+			if err != nil {
+				return err
+			}
+		case reflect.Int64:
+			err := setInt64(&instance, name)
+			if err != nil {
+				return err
+			}
+
+		case reflect.Uint:
+			err := setUint(&instance, name)
+			if err != nil {
+				return err
+			}
+		case reflect.Uint8:
+			err := setUint8(&instance, name)
+			if err != nil {
+				return err
+			}
+		case reflect.Uint16:
+			err := setUint16(&instance, name)
+			if err != nil {
+				return err
+			}
+		case reflect.Uint32:
+			err := setUint32(&instance, name)
+			if err != nil {
+				return err
+			}
+		case reflect.Uint64:
+			err := setUint64(&instance, name)
+			if err != nil {
+				return err
+			}
+		case reflect.Float32:
+			err := setFloat32(&instance, name)
+			if err != nil {
+				return err
+			}
+		case reflect.Float64:
+			err := setFloat64(&instance, name)
+			if err != nil {
+				return err
+			}
+		case reflect.Bool:
+			err := setBool(&instance, name)
+			if err != nil {
+				return err
+			}
+		} // switch
+	} // for
+
+	return nil
+}
+
+// setInt try to set `int` to instance.
+func setInt(instance *reflect.Value, name string) error {
+	// Parse value from the environment.
+	r, err := getInt64(Get(name))
+	if err != nil {
+		return err
+	}
+
+	if strconv.IntSize == 32 && math.MaxInt32 < r {
+		return fmt.Errorf("%s \"%d\": value out of range", name, r)
+	}
+
+	if strconv.IntSize == 64 && math.MaxInt64 < r {
+		return fmt.Errorf("%s \"%d\": value out of range", name, r)
+	}
+
+	instance.SetInt(r)
+	return nil
+}
+
+// setInt8 try to set `int8` to instance.
+func setInt8(instance *reflect.Value, name string) error {
+	// Parse value from the environment.
+	r, err := getInt64(Get(name))
+	if err != nil {
+		return err
+	}
+
+	if math.MaxInt8 < r {
+		return fmt.Errorf("%s \"%d\": value out of range", name, r)
+	}
+
+	instance.SetInt(r)
+	return nil
+}
+
+// setInt16 try to set `int16` to instance.
+func setInt16(instance *reflect.Value, name string) error {
+	// Parse value from the environment.
+	r, err := getInt64(Get(name))
+	if err != nil {
+		return err
+	}
+
+	if math.MaxInt16 < r {
+		return fmt.Errorf("%s \"%d\": value out of range", name, r)
+	}
+
+	instance.SetInt(r)
+	return nil
+}
+
+// setInt32 try to set `int32` to instance.
+func setInt32(instance *reflect.Value, name string) error {
+	// Parse value from the environment.
+	r, err := getInt64(Get(name))
+	if err != nil {
+		return err
+	}
+
+	if math.MaxInt32 < r {
+		return fmt.Errorf("%s \"%d\": value out of range", name, r)
+	}
+
+	instance.SetInt(r)
+	return nil
+}
+
+// setInt64 try to set `int64` to instance.
+func setInt64(instance *reflect.Value, name string) error {
+	// Parse value from the environment.
+	r, err := getInt64(Get(name))
+	if err != nil {
+		return err
+	}
+
+	instance.SetInt(r)
+	return nil
+}
+
+// setUint try to set `uint` to instance.
+func setUint(instance *reflect.Value, name string) error {
+	// Parse value from the environment.
+	r, err := getUint64(Get(name))
+	if err != nil {
+		return err
+	}
+
+	if strconv.IntSize == 32 && math.MaxUint32 < r {
+		return fmt.Errorf("%s \"%d\": value out of range", name, r)
+	}
+
+	if strconv.IntSize == 64 && math.MaxUint64 < r {
+		return fmt.Errorf("%s \"%d\": value out of range", name, r)
+	}
+
+	instance.SetUint(r)
+	return nil
+}
+
+// setUint8 try to set `uint8` to instance.
+func setUint8(instance *reflect.Value, name string) error {
+	// Parse value from the environment.
+	r, err := getUint64(Get(name))
+	if err != nil {
+		return err
+	}
+
+	if math.MaxUint8 < r {
+		return fmt.Errorf("%s \"%d\": value out of range", name, r)
+	}
+
+	instance.SetUint(r)
+	return nil
+}
+
+// setUint16 try to set `uint16` to instance.
+func setUint16(instance *reflect.Value, name string) error {
+	// Parse value from the environment.
+	r, err := getUint64(Get(name))
+	if err != nil {
+		return err
+	}
+
+	if math.MaxUint16 < r {
+		return fmt.Errorf("%s \"%d\": value out of range", name, r)
+	}
+
+	instance.SetUint(r)
+	return nil
+}
+
+// setUint32 try to set `uint32` to instance.
+func setUint32(instance *reflect.Value, name string) error {
+	// Parse value from the environment.
+	r, err := getUint64(Get(name))
+	if err != nil {
+		return err
+	}
+
+	if math.MaxUint32 < r {
+		return fmt.Errorf("%s \"%d\": value out of range", name, r)
+	}
+
+	instance.SetUint(r)
+	return nil
+}
+
+// setUint64 try to set `uint64` to instance.
+func setUint64(instance *reflect.Value, name string) error {
+	// Parse value from the environment.
+	r, err := getUint64(Get(name))
+	if err != nil {
+		return err
+	}
+
+	instance.SetUint(r)
+	return nil
+}
+
+// setFloat32 try to set `float32` to instance.
+func setFloat32(instance *reflect.Value, name string) error {
+	// Parse value from the environment.
+	r, err := getFloat64(Get(name))
+	if err != nil {
+		return err
+	}
+
+	if math.MaxFloat32 < r {
+		return fmt.Errorf("%s \"%f\": value out of range", name, r)
+	}
+
+	instance.SetFloat(r)
+	return nil
+}
+
+// setFloat64 try to set `float64` to instance.
+func setFloat64(instance *reflect.Value, name string) error {
+	// Parse value from the environment.
+	r, err := getFloat64(Get(name))
+	if err != nil {
+		return err
+	}
+
+	instance.SetFloat(r)
+	return nil
+}
+
+// setBool try to set `bool` to instance.
+func setBool(instance *reflect.Value, name string) error {
+	// Parse value from the environment.
+	v := Get(name)
+	if len(v) == 0 {
+		instance.SetBool(false)
+		return nil
+	}
+
+	// Try parse.
+	b, err := strconv.ParseBool(v)
+	if err != nil {
+		f, errF := strconv.ParseFloat(v, 64)
+		if errF != nil {
+			return err
+		}
+
+		if math.Abs(f) > 1e-9 {
+			b = true
+		}
+	}
+
+	instance.SetBool(b)
+	return nil
+}
---
+++ decode_test.go
@@ -0,0 +1,316 @@
+package env
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+type Data struct {
+	Host  string `env:"HOST"`
+	Port  uint   `env:"PORT,"`
+	Paths string `env:"PATH,:"`
+}
+
+type DataNumber struct {
+	KeyInt     int     `env:"KEY_INT"`
+	KeyInt8    int8    `env:"KEY_INT8"`
+	KeyInt16   int16   `env:"KEY_INT16"`
+	KeyInt32   int32   `env:"KEY_INT32"`
+	KeyInt64   int64   `env:"KEY_INT64"`
+	KeyUint    uint    `env:"KEY_UINT"`
+	KeyUint8   uint8   `env:"KEY_UINT8"`
+	KeyUint16  uint16  `env:"KEY_UINT16"`
+	KeyUint32  uint32  `env:"KEY_UINT32"`
+	KeyUint64  uint64  `env:"KEY_UINT64"`
+	KeyFloat32 float32 `env:"KEY_FLOAT32"`
+	KeyFloat64 float64 `env:"KEY_FLOAT64"`
+}
+
+type DataBool struct {
+	KeyBool bool `env:"KEY_BOOL"`
+}
+
+// TestGetInt64 tests getInt64 function.
+func TestGetInt64(t *testing.T) {
+	var (
+		v   int64
+		err error
+	)
+
+	// Parse incorrect value.
+	_, err = getInt64("string")
+	if err == nil {
+		t.Error("getInt64: didn't handle the error")
+	}
+
+	// Parse empty string.
+	v, err = getInt64("")
+	if err != nil {
+		t.Error("getInt64: to raise an exception on an empty string")
+	}
+
+	if v != 0 {
+		t.Error("getInt64: has an invalid default value")
+	}
+
+	// Parse correct value.
+	v, err = getInt64("3")
+	if err != nil {
+		t.Errorf("getInt64: %v", err)
+	}
+
+	if v != 3 {
+		t.Errorf("getInt64: incorrect value: `%d`!=3", v)
+	}
+
+	// Parse to long value.
+	_, err = getInt64("922337203685477580777")
+	if err == nil {
+		t.Error("getInt64: the `value out of range` error was ignored")
+	}
+
+}
+
+// TestGetUint64 tests getUint64 function.
+func TestGetUint64(t *testing.T) {
+	var (
+		v   uint64
+		err error
+	)
+
+	// Parse incorrect value.
+	_, err = getUint64("string")
+	if err == nil {
+		t.Error("didn't handle the error")
+	}
+
+	// Parse empty string.
+	v, err = getUint64("")
+	if err != nil {
+		t.Error("to raise an exception on an empty string")
+	}
+
+	if v != 0 {
+		t.Error("has an invalid default value")
+	}
+
+	// Parse correct value.
+	v, err = getUint64("3")
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	if v != 3 {
+		t.Errorf("incorrect value: `%d`!=3", v)
+	}
+
+	// Parse to long value.
+	_, err = getUint64("922337203685477580777")
+	if err == nil {
+		t.Error("the `value out of range` error was ignored")
+	}
+
+}
+
+// TestGetFloat64 tests getFloat64 function.
+func TestGetFloat64(t *testing.T) {
+	var (
+		v   float64
+		err error
+	)
+
+	// Parse incorrect value.
+	_, err = getFloat64("string")
+	if err == nil {
+		t.Error("didn't handle the error")
+	}
+
+	// Parse empty string.
+	v, err = getFloat64("")
+	if err != nil {
+		t.Error("to raise an exception on an empty string")
+	}
+
+	if v != 0 {
+		t.Error("has an invalid default value")
+	}
+
+	// Parse correct value.
+	v, err = getFloat64("3.0")
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	if v != 3.0 {
+		t.Errorf("incorrect value: `%f`!=3.0", v)
+	}
+}
+
+// TestParseTag tests parseTag function.
+func TestParseTag(t *testing.T) {
+	var tests = [][]string{
+		[]string{"HOST", "host", " ", "HOST", " "},
+		[]string{"PATHS,:", "paths", " ", "PATHS", ":"},
+		[]string{",:", "PORT", " ", "PORT", ":"},
+		[]string{",", "PORT", ":", "PORT", ":"},
+	}
+
+	for _, test := range tests {
+		name, sep := parseTag(test[0], test[1], test[2])
+		if test[3] != name {
+			t.Errorf("incorrect value for name `%s`!=`%s`", test[3], name)
+		}
+
+		if test[4] != sep {
+			t.Errorf("incorrect value for sep `%s`!=`%s`", test[4], sep)
+		}
+	}
+}
+
+// TestDecodeEnvironNumber tests decodeEnviron function
+// for Int, Uint and Float types.
+func TestDeocdeEnvironNumber(t *testing.T) {
+	var max = "922337203685477580777"
+	var tests = map[string][]string{
+		"KEY_INT":     []string{"2", "-2", max},
+		"KEY_INT8":    []string{"8", "-8", max},
+		"KEY_INT16":   []string{"16", "-16", max},
+		"KEY_INT32":   []string{"32", "-32", max},
+		"KEY_INT64":   []string{"64", "-64", max},
+		"KEY_UINT":    []string{"2", "-2", max},
+		"KEY_UINT8":   []string{"8", "-8", max},
+		"KEY_UINT16":  []string{"16", "-16", max},
+		"KEY_UINT32":  []string{"32", "-32", max},
+		"KEY_UINT64":  []string{"64", "-64", max},
+		"KEY_FLOAT32": []string{"32.0", "-32.0", max},
+		"KEY_FLOAT64": []string{"64.0", "-64.0", max},
+	}
+
+	// Correct value.
+	for i := 0; i < 3; i++ {
+		for key, data := range tests {
+			var d = &DataNumber{}
+
+			Clear()
+			Set(key, data[i])
+
+			err := decodeEnviron(d)
+			switch i {
+			case 0:
+				if err != nil {
+					t.Error(err)
+				}
+			case 1:
+				if !strings.Contains(key, "UINT") {
+					// Int and float types.
+					if err != nil {
+						t.Error(err)
+					}
+				} else {
+					// Uint cannot be negative.
+					if err == nil {
+						t.Errorf("uint cannot be negative: %s", data[i])
+					}
+					continue
+				}
+			case 2:
+				// Ignore FloatX to check for `value out of range`.
+				if !strings.Contains(key, "FLOAT") {
+					if err == nil {
+						t.Errorf("for %s must be `value out of "+
+							"range` exception", key)
+					}
+				}
+				continue
+			}
+
+			switch key {
+			case "KEY_INT":
+				if v := fmt.Sprintf("%d", d.KeyInt); v != data[i] {
+					t.Errorf("value isn't correct `%s`!=`%s`", v, data[i])
+				}
+			case "KEY_INT8":
+				if v := fmt.Sprintf("%d", d.KeyInt8); v != data[i] {
+					t.Errorf("value isn't correct `%s`!=`%s`", v, data[i])
+				}
+			case "KEY_INT16":
+				if v := fmt.Sprintf("%d", d.KeyInt16); v != data[i] {
+					t.Errorf("value isn't correct `%s`!=`%s`", v, data[i])
+				}
+			case "KEY_INT32":
+				if v := fmt.Sprintf("%d", d.KeyInt32); v != data[i] {
+					t.Errorf("value isn't correct `%s`!=`%s`", v, data[i])
+				}
+			case "KEY_INT64":
+				if v := fmt.Sprintf("%d", d.KeyInt64); v != data[i] {
+					t.Errorf("value isn't correct `%s`!=`%s`", v, data[i])
+				}
+			case "KEY_UINT":
+				if v := fmt.Sprintf("%d", d.KeyUint); v != data[i] {
+					t.Errorf("value isn't correct `%s`!=`%s`", v, data[i])
+				}
+			case "KEY_UINT8":
+				if v := fmt.Sprintf("%d", d.KeyUint8); v != data[i] {
+					t.Errorf("value isn't correct `%s`!=`%s`", v, data[i])
+				}
+			case "KEY_UINT16":
+				if v := fmt.Sprintf("%d", d.KeyUint16); v != data[i] {
+					t.Errorf("value isn't correct `%s`!=`%s`", v, data[i])
+				}
+			case "KEY_UINT32":
+				if v := fmt.Sprintf("%d", d.KeyUint32); v != data[i] {
+					t.Errorf("value isn't correct `%s`!=`%s`", v, data[i])
+				}
+			case "KEY_UINT64":
+				if v := fmt.Sprintf("%d", d.KeyUint64); v != data[i] {
+					t.Errorf("value isn't correct `%s`!=`%s`", v, data[i])
+				}
+			} // switch
+		}
+	}
+}
+
+// TestDecodeEnvironBoll tests decodeEnviron function for bool type.
+func TestDeocdeEnvironBool(t *testing.T) {
+	var tests = map[string]bool{
+		"true":  true,
+		"false": false,
+		"0":     false,
+		"1":     true,
+		"":      false,
+		"True":  true,
+		"TRUE":  true,
+		"False": false,
+		"FALSE": false,
+	}
+
+	for value, test := range tests {
+		var d = &DataBool{}
+
+		Clear()
+		Set("KEY_BOOL", value)
+
+		err := decodeEnviron(d)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if d.KeyBool != test {
+			t.Errorf("KeyBool == %t but nedd %t", d.KeyBool, test)
+		}
+	}
+
+	// Incorrect value.
+	for _, value := range []string{"string", "0.d", "true/false"} {
+		var d = &DataBool{}
+
+		Clear()
+		Set("KEY_BOOL", value)
+
+		err := decodeEnviron(d)
+		if err == nil {
+			t.Error("didn't handle the error")
+		}
+	}
+}
